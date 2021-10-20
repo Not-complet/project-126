c08c44d76da689af068baec2df6632caaca3baa1
Not-complet <77133545+Not-complet@users.noreply.github.com>
2021-10-19 21:15:19 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = "";
+rightWristX = 0;
+leftWristX = 0;
+rightWristY = 0;
+leftWristY = 0;
 function preload(){
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3"); 
@@ -9,7 +13,20 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose",gotPoses);
 }
 function draw(){
     image(video,0,0,600,500);
+}
+function modelLoaded(){
+    console.log("PoseNet Loaded!");
+}
+function gotPoses(results){
+    if(results.length > 0){
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        leftWristY = results[0].pose.leftWrist.y;
+    }
 }
