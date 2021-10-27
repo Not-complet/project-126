88805b896b32becc7a5e4cac5404912140b092e7
Not-complet <77133545+Not-complet@users.noreply.github.com>
2021-10-26 21:39:21 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ var leftWristX = 0;
 var rightWristY = 0;
 var leftWristY = 0;
 var leftWristScore = 0;
+var rightWristScore = 0;
 var song_status1 = "";
+var song_status2 = "";
 function preload(){
     song1 = loadSound("music.mp3");
     song2 = loadSound("music2.mp3"); 
@@ -23,12 +25,21 @@ function draw(){
     fill("#14deca");
     stroke("#14deca");
     song_status1 = song1.isPlaying();
+    song_status2 = song2.isPlaying();
     if(leftWristScore > 0.2){
         circle(leftWristX, leftWristY, 20);
         song2.stop();
         if(song_status1 == false){
             song1.play();
             document.getElementById("song").innerHTML = "Current Song: Song 1 (left wrist)";
+        }
+    }
+    if(rightWristScore > 0.2){
+        circle(leftWristX, leftWristY, 20);
+        song1.stop();
+        if(song_status2 == false){
+            song2.play();
+            document.getElementById("song").innerHTML = "Current Song: song2 (right wrist)";
         }
     }
 }
@@ -42,8 +53,10 @@ function gotPoses(results){
         rightWristY = results[0].pose.rightWrist.y;
         leftWristY = results[0].pose.leftWrist.y;
         leftWristScore = results[0].pose.keypoints[9].score;
+        rightWristScore = results[0].pose.keypoints[10].score;
         console.log("left wrist x and y: " + leftWristX + ", "+ leftWristY);
         console.log("right wrist x and y: "+ rightWristX +  ", " + rightWristY);
-        console.log("left wrist score: " + leftWristScore );
+        console.log("left wrist score: " + leftWristScore);
+        console.log("right wrist score: " + rightWristScore);
     }
 }
